633c04f3226f348ce734465798cf3265ca9801a1
John Micheal Acera <john.acera@sportality.se>
2025-08-09 19:23:46 +0800

    modify to pnpm workspace

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,20 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '..', '..');
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: new Proxy({}, {
+    get: (target, name) => path.join(projectRoot, 'node_modules', name),
+  }),
+};
+
+module.exports = config;
+
+
